5892b2232557dd1e7865f9a8e5ffad3ad80d0f6b
KVN06 <160743956+KVN06@users.noreply.github.com>
2025-09-24 17:52:45 -0500

    ...

---
+++ src/app/pages/products/products.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.html',
+  styleUrls: ['./products.css']
+})
+export class Products {
+}
